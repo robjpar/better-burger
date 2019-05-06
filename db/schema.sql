4b1fa50947af9212637dc862d01970083e5ad1cd
Robert J. Paruch <roobbb@live.com>
2019-05-06 00:28:07 -0700

    Recreated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,3 @@
--- Structure of the database
-
-CREATE DATABASE IF NOT EXISTS burger_db;
-
-USE burger_db;
-
-CREATE TABLE IF NOT EXISTS burgers (
-    burger_id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(100) NOT NULL,
-    delivered BOOLEAN DEFAULT FALSE,
-    ordered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    delivered_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (burger_id)
-);
+-- Create/reset the database
+DROP DATABASE IF EXISTS better_db;
+CREATE DATABASE better_db;
